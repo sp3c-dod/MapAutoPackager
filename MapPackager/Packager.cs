7684d6a6a352f197392bd4937a8d5cf6875d274a
Bill Kammermeier <clarksfan316@yahoo.com>
2022-05-10 14:29:26 -0400

    Parse RESGen output and store into a string[] of file paths
    Added parsing of the RESGen.exe output and store into a string[] of file paths
    Raw RESGen.exe output will also be captured
    Added error messages around possible failures of parsing the RESGen.exe output

--- MapPackager/Packager.cs
+++ MapPackager/Packager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace MapPackager
 {
@@ -9,14 +10,23 @@ namespace MapPackager
     /// </summary>
     public class Packager
     {
-        public string[] GameDirectories { get; set; }
-        public string ZipOutputDirectory { get; set; }
-        public string PathToResGenExecutable { get; set; }
-
+        //TODO: move these to a configuration file
         // -g: outputs file list to the console
         // -o: overwrites any existing .res file
         // -f: specifically the map file
-        private const string ResGenParameters = " -go -f \"{0}\"";
+        private const string ResGenParameters = " -gok -f \"{0}\"";
+        private const string ResGenExecutableFilename = "RESGen.exe";
+        private const string StartOfFileListToken = "Creating";
+        private const string EndOfFileListSuccessToken = "Done creating res file(s)!";
+
+        // Error messages
+        private const string ResGenCreateMessageNotFound = "Never found message saying RESGen.exe started creating the .res file";
+        private const string ProcessDidNotStart = "Process Did not Start Succesfully. process.Start() returned false";
+        private const string NeverFoundResGenEndMessage = "Never found end message from RESGen.exe saying .res file was done being created.";
+
+        public string[] GameDirectories { get; set; }
+        public string ZipOutputDirectory { get; set; }
+        public string PathToResGenExecutable { get; set; }
 
         /// <summary>
         /// 
@@ -27,6 +37,7 @@ namespace MapPackager
         {
             //TODO:
             // Generate Resource File
+            GenerateResourceFile(Path.Combine(GameDirectories[0], bspName));
             // Parse resource file and load resources into memory
             // Remove entries that are default files that come with the game
             // Store this as a text file, so that it can be changed (maybe even used with other games)
@@ -34,7 +45,7 @@ namespace MapPackager
             // Add optional entries for things such .cfg and overview files
             // Search through provided folder structures using the order as priority and generate a list of which files exist and which don't
             // Create ZIP and save to zipOutputDirectory
-            GenerateResourceFile(Path.Combine(GameDirectories[0], bspName));
+
             return String.Empty;
         }
 
@@ -48,35 +59,88 @@ namespace MapPackager
         /// <returns></returns>
         private ResourceFileResult GenerateResourceFile(string bspPath)
         {
-            var proc = new Process
+            var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
                     // The original host website of RESGen.exe no longer appears to be up as of May 5, 2022, but I 
                     // did find the source code for it on GitHub:
                     // https://github.com/kriswema/resgen
-                    FileName = Path.Combine(PathToResGenExecutable, "RESGen.exe"),
-                    // -k skips the need to press enter to close the program
-                    // Add double quotes around file path otherwise a space in the folder path will fail
+                    FileName = Path.Combine(PathToResGenExecutable, ResGenExecutableFilename),
                     Arguments = String.Format(ResGenParameters, bspPath), 
-                    UseShellExecute = true,
-                    RedirectStandardOutput = false,
-                    CreateNoWindow = false,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    ErrorDialog = false,
+                    WindowStyle = ProcessWindowStyle.Hidden,
                     WorkingDirectory = PathToResGenExecutable
                 }
             };
 
             ResourceFileResult resourceFileResult = new ResourceFileResult();
+            StringBuilder rawResGenOutput = new StringBuilder();
             try
             {
-                resourceFileResult.SuccessfullyGenerated = proc.Start();
-                //TODO: find a way to capture output. add a verbose option?
-                //StandardError
-                //StandardOutput
+                bool startedWithoutError = process.Start();
+
+                if (!startedWithoutError)
+                {
+                    resourceFileResult.SuccessfullyGenerated = false;
+                    resourceFileResult.ErrorMessage = ProcessDidNotStart;
+                    return resourceFileResult;
+                }
+
+                StringBuilder resourceList = new StringBuilder();
+
+                string currentLine;
+                bool foundStartOfFileList = false;
+                bool foundSuccessMessageAtEnd = false;
+                while ((currentLine = process.StandardOutput.ReadLine()) != null)
+                {
+                    rawResGenOutput.AppendLine(currentLine);
+                    if (!foundStartOfFileList && currentLine.StartsWith(StartOfFileListToken))
+                    {
+                        foundStartOfFileList = true;
+                        continue;
+                    }
+                    else if (foundStartOfFileList && !String.IsNullOrWhiteSpace(currentLine))
+                    {
+                        if (!currentLine.StartsWith(EndOfFileListSuccessToken))
+                        {
+                            resourceList.AppendLine(currentLine.Trim());
+                        }
+                        else
+                        {
+                            foundSuccessMessageAtEnd = true;
+                        }
+                        
+                    }
+                }
+
+                process.WaitForExit();
+                resourceFileResult.RawResGenOutput = rawResGenOutput.ToString();
+
+                // Trim off the find new line since each line was added with AppendLine including the last
+                resourceFileResult.ResGenFileList = resourceList.ToString().Trim().Split(Environment.NewLine);
+
+                if (!foundStartOfFileList)
+                {
+                    resourceFileResult.ErrorMessage = ResGenCreateMessageNotFound;
+                    
+                    resourceFileResult.SuccessfullyGenerated = false;
+                }
+                else if (!foundSuccessMessageAtEnd)
+                {
+                    resourceFileResult.ErrorMessage = NeverFoundResGenEndMessage;
+                    resourceFileResult.SuccessfullyGenerated = false;
+                }
+
+                resourceFileResult.SuccessfullyGenerated = true;
             }
             catch(Exception ex)
             {
-                Debug.WriteLine(ex.Message);
+                resourceFileResult.ErrorMessage = ex.Message;
+                resourceFileResult.RawResGenOutput = rawResGenOutput.ToString();
                 resourceFileResult.SuccessfullyGenerated = false;
             }
 
--- MapPackager/ResourceFileResult.cs
+++ MapPackager/ResourceFileResult.cs
@@ -1,8 +1,28 @@
 ﻿namespace MapPackager
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ResourceFileResult
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public bool SuccessfullyGenerated { get; set; }
-        public string PathToResourceFile { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string[] ResGenFileList { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string RawResGenOutput { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ErrorMessage { get; set; }
     }
 }
